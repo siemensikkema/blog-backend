944e08eb7cb41669f60133f652c423d0beab66f4
Siemen Sikkema <siemen@unclutter.nl>
2016-07-23 13:58:58 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+	name: "blog-backend",
+	dependencies: [
+		.Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 14),
+	]
+)
