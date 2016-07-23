42275b524218001a104e915d976d90fb65428b24
Siemen Sikkema <siemen@unclutter.nl>
2016-07-23 15:06:32 +0200

    Add App target and build settings

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
 	name: "blog-backend",
 	dependencies: [
 		.Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 14),
-	]
+	],
+	exclude: ["XCConfig"]
 )
